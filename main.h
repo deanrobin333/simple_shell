c907c80de344f54f0297dc45c2d9fb1578647ae6
deanrobin333 <deanrobin777@gmail.com>
2023-05-22 00:21:54 +0000

    split child fork in prompt-user

--- main.h
+++ main.h
@@ -3,5 +3,6 @@
 #define MAX_COMMANDS 10
 
 void prompt_user(char **, char **);
+void child(char *command_ptr, char *execve_argv[], char *argv[], char *envp[]);
 #endif
 
--- prompt_user.c
+++ prompt_user.c
@@ -7,6 +7,43 @@
 #include <string.h>
 
 /**
+ * child - function to create child process
+ * @command_ptr: pointer containing commands from stdin
+ * @execve_argv: pointer containing token commands
+ * @argv: pointer to array of arguments
+ * @envp: pointer to environments
+ * Return: void
+ */
+
+void child(char *command_ptr, char *execve_argv[], char *argv[], char *envp[])
+{
+	pid_t execve_child_pid;
+	int child_status;
+
+	execve_child_pid = fork();
+	if (execve_child_pid == -1)
+	{
+		perror("fork");
+		free(command_ptr);
+		exit(1);
+	}
+	if (execve_child_pid == 0)
+	{
+		execve(execve_argv[0], execve_argv, envp);
+		if (execve(execve_argv[0], execve_argv, envp) == -1)
+		{
+			printf("%s: No such file or directory\n", argv[0]);
+		}
+
+	}
+	else
+	{
+		wait(&child_status);
+	}
+
+}
+
+/**
  * prompt_user - prompts the user for a command
  * @argv: pointer to command line arguments
  * @envp: pointer to environment paths
@@ -19,8 +56,7 @@ void prompt_user(char *argv[], char *envp[])
 	char *command_ptr = NULL, *execve_argv[MAX_COMMANDS];
 	ssize_t command_char;
 	size_t byte_size = 0;
-	int i, child_status;
-	pid_t execve_child_pid;
+	int i;
 
 	for (;;)
 	{
@@ -37,25 +73,14 @@ void prompt_user(char *argv[], char *envp[])
 			if (command_ptr[i] == '\n')
 				command_ptr[i] = '\0';
 		}
-		if (*command_ptr != '\0') 
+		if (*command_ptr != '\0')
 		{
-		execve_argv[0] = strtok(command_ptr, " ");
-		for (i = 0; execve_argv[i] != NULL;)
-			execve_argv[++i] = strtok(NULL, " ");
-		execve_child_pid = fork();
-		if (execve_child_pid == -1)
-		{
-			free(command_ptr);
-			exit(1);
-		}
-		if (execve_child_pid == 0)
-		{
-			if (execve(execve_argv[0], execve_argv, envp) == -1)
-				printf("%s: No such file or directory\n", argv[0]);
-		}
-		else
-			wait(&child_status);
+			execve_argv[0] = strtok(command_ptr, " ");
+			for (i = 0; execve_argv[i] != NULL;)
+				execve_argv[++i] = strtok(NULL, " ");
+			child(command_ptr, execve_argv, argv, envp);
 		}
 	}
+	free(command_ptr);
 }
 
