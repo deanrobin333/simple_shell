2d3c91e7b40163455eef7bf420baf9d74310a09d
deanrobin333 <deanrobin777@gmail.com>
2023-05-22 04:08:03 +0000

    created separate file for forking; split from prompt_user.c

---
+++ fork_child.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/**
+ * child - function to create child process
+ * @command_ptr: pointer containing commands from stdin
+ * @execve_argv: pointer containing token commands
+ * @argv: pointer to array of arguments
+ * @envp: pointer to environments
+ * Return: void
+ */
+
+int child(char *command_ptr, char *execve_argv[], char *argv[], char *envp[])
+{
+	pid_t execve_child_pid;
+	int child_status;
+
+	execve_child_pid = fork();
+	if (execve_child_pid == -1)
+	{
+		free(command_ptr);
+		perror("fork");
+		exit(1);
+	}
+	if (execve_child_pid == 0)
+	{
+		if (execve(execve_argv[0], execve_argv, envp) == -1)
+		{
+			printf("%s: No such file or directory\n", argv[0]);
+			exit(0);
+		}
+	}
+	else
+		wait(&child_status);
+	return (WIFEXITED(child_status) && WEXITSTATUS(child_status));
+
+}
+
