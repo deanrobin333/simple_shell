a8f4f37159f4155c027b8cc5f7137f890fa6036b
deanrobin333 <deanrobin777@gmail.com>
2023-05-24 03:35:21 +0000

    exit function- betty

--- my_exit.c
+++ my_exit.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include "main.h"
+/**
+ * my_exit - handles arguments for exit
+ * @argv: pointer to commands
+ * Return: void
+ */
 
 void my_exit(char *argv)
 {
-    if (argv != NULL)
-    {
-        int status = atoi(argv);
-        exit(status);
-    }
-    else
-    {
-        exit(0);
-    }
+	int status;
+
+	if (argv != NULL)
+	{
+		status = atoi(argv);
+		exit(status);
+	}
+	else
+		exit(0);
 }
 
